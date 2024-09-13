6a159c92b109dfe116359bbc44f887cb2b9a8a3b
issaabubakry <issaabubakry@gmail.com>
2024-09-13 13:29:13 -0700

    indicate a if statement execute some function if password is correct

--- process.php
+++ process.php
@@ -11,5 +11,11 @@ $stmt->bindParam(":username",username);
 $stmt->execute();
 
 if($stmt->rowCount() > 0) {
-    
+    $row = $stmt->fetch();
+    if($row['password']==$password) {
+        $_SESSION['aid']- $row['aid'];
+        $_SESSION['admin_id']- $username;
+        $_SESSION['aname']- $row['aname'];
+        header("location: admin-dashboard.php");
+    }
 }
