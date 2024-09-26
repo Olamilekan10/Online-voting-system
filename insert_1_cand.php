301a29237823f6ff5970962422c9295cca534ffb
issaabubakry <issaabubakry@gmail.com>
2024-09-25 21:03:32 -0700

    added some navbar and header link to first candidate enroll

--- insert_1_cand.php
+++ insert_1_cand.php
@@ -14,13 +14,44 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- bootstrap css icon cdn -->
 
-
     <!-- custom css -->
     <link rel="stylesheet" href="css/style.css">
     <!-- custom css -->
 </head>
 
 <body>
+
+    <!-- Header nav -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Go Vote</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav_li active">
+                            <a class="nav_a" href="index.html">Home <span class="sr-only"></span></a>
+                        </li>
+                        <li class="nav_li">
+                            <a class="nav_a" href="year.html">Candidate</a>
+                        </li>
+                        <li class="nav_li">
+                            <a class="nav_a" href="#">Result</a>
+                        </li>
+                        <li class="nav_li">
+                            <a class="nav_a" href="#">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- Header nav -->
 
     <div class="container">
         <div class="row">
---
+++ insert_2_cand.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+     <!-- bootstrap css cdn -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <!-- bootstrap css icon cdn -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- bootstrap css icon cdn -->
+    
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- custom css -->
+     
+</head>
+<body>
+    
+</body>
+</html>
